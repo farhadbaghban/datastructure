0c5615c80bd7fa6d0bf405c6b6e73ddbe716393c
farhadbaghban <baghbanfarhad@gmail.com>
2020-10-15 03:45:30 +0330

    class created

---
+++ stack.py
@@ -0,0 +1,24 @@
+# stack
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+    # i use the append because, append add the value at last
+    # as well as , i can use the extend
+    # append can take more than 1 value(in this case we dont need it)
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+    # peek get the top member from the stack BUT dont remove
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
